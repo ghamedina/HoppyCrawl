b8085a59b021591370e9a240b6a3266f969da64c
cabelhigh <cabelhigh@yahoo.com>
2017-04-10 17:14:04 -0700

    Public Crawl section added to Crawls index. Your crawls can be made public

--- app/controllers/crawls_controller.rb
+++ app/controllers/crawls_controller.rb
@@ -42,6 +42,7 @@ class CrawlsController < ApplicationController
   def create
     @crawl = Crawl.new
     if @crawl.name.blank?
+      @crawl.shareable = false
       @crawl.name = "Default"
     else
       @crawl.name = params[:name]
@@ -61,7 +62,12 @@ class CrawlsController < ApplicationController
         format.json { render json: @crawl.errors, status: :unprocessable_entity }
       end
     end
+  end
 
+  def make_shareable
+    @crawl.shareable = true
+    @crawl.save
+    redirect_to '/crawls'
   end
 
   # PATCH/PUT /crawls/1
@@ -113,6 +119,6 @@ class CrawlsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def crawl_params
-      params.require(:crawl).permit(:name, :address, :start_time, :end_time, :date, :brewery_stops, brewery_stops_attributes: [:start_time, :end_time, :id, :brewery_id, :crawl_id]) #this won't work if show and create are not in load_and_authorize_resource
+      params.require(:crawl).permit(:name, :address, :start_time, :end_time, :date, :shareable, :brewery_stops, brewery_stops_attributes: [:start_time, :end_time, :id, :brewery_id, :crawl_id]) #this won't work if show and create are not in load_and_authorize_resource
     end
 end
